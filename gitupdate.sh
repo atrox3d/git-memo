07ff23893dbc4ce0c35a41cf3b2f55e1437f2ab4
atrox3d <atrox3d@gmail.com>
2018-07-26 06:52:44 +0200

    improve output

--- gitupdate.sh
+++ gitupdate.sh
@@ -35,17 +35,17 @@ do
 	printf -v TAG "[%-30.30s]" "$DIR"
 	# subshell
 	(
+		EXTRA=
 		# let's move into
 		cd "$DIR"
 		# do we have remotes?
 		[ "$(git remote -v)" != "" ] && {
 			# yes, then we fetch
 			git fetch > /dev/null
-			NOREMOTES=
 		} || {
 			# no, we dont
-			NOREMOTES="${COLOR_REV_RED}* no remotes available * ${COLOR_OFF}"
-			#echo -en "$TAG	${COLOR_REV_RED}*** no remotes available ***${COLOR_OFF}"
+			#EXTRA="${COLOR_REV_RED}* no remotes available * ${COLOR_OFF}"
+			printf -v EXTRA "[%-25.25s]" "no remotes available"
 		}
 		# git output
 		STATUS="$(git status 2>&1 )"
@@ -60,26 +60,31 @@ do
 			#
 			#egrep -qiz '(On branch.*)(Your branch is up-to-date with.*)*nothing to commit, working tree clean' <<< "$STATUS" && {
 			#git status |  tr $'\n' ' ' | egrep -i "(On branch [^[:space:]]+)[[:space:]]+(your branch is up to date with '[^']+'\.)[[:space:]]+(nothing to commit, working tree clean)"
-			regex="(On branch [^[:space:]]+)[[:space:]]+(your branch is up[ -]to[ -]date with '[^']+'\.[[:space:]]+)*(nothing to commit, working tree clean)"
+			regex="(On branch [^[:space:]]+)[[:space:]]+"
+			regex="${regex}(your branch is up[ -]to[ -]date with '[^']+'\.[[:space:]]+)*"
+			regex="${regex}(nothing to commit, working tree clean)"
+			
 			git status | tr $'\n' ' ' | egrep -qi "$regex" && {
-				printf "$TAG	${NOREMOTES}${COLOR_REV_GREEN}%-25.25s${COLOR_OFF}\n" "ok"
+				printf "$TAG	${COLOR_REV_GREEN}%-25.25s${COLOR_OFF}${EXTRA}\n" "ok"
 				#echo "$STATUS"
 				#git status
 			} || {
 				#
 				#	something to do
 				#
-				printf "$TAG	${NOREMOTES}${COLOR_REV_YELLOW}%-25.25s${COLOR_OFF}\n" "check messages"
+				printf "$TAG	${COLOR_REV_YELLOW}%-25.25s${COLOR_OFF}${EXTRA}\n" "check messages"
+				echo "----------------------------------------------------------------------------"
 				git status
-				echo
+				echo "----------------------------------------------------------------------------"
 			}
 		} || {
 			#
 			#	ERROR!!!!
 			#
-			printf "$TAG	${NOREMOTES}${COLOR_REV_RED}%-25.25s${COLOR_OFF}\n" "something's wrong"
+			printf "$TAG	${COLOR_REV_RED}%-25.25s${COLOR_OFF}${EXTRA}\n" "something's wrong"
+			echo "----------------------------------------------------------------------------"
 			git status
-			echo
+			echo "----------------------------------------------------------------------------"
 		}
 	)
 	#
